9ab8fcb6fcdbe00ca28483f2731e63107b4cb359
Jonathan Huston <jonathan.e.huston@gmail.com>
2023-05-08 14:19:41 -0700

    Actual initial build. Can take a list of cities, a single variable, and return JSON with results

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # American_City_Data
 CS 361 Project - Compare and visualize data American cities
+
+Data is sourced from the 2021 American Community Survey.
+For more information about using the Census Data API, see here: 
+https://www.census.gov/data/developers/guidance/api-user-guide.Available_Data.html#list-tab-2080675447
+
+A list of available datasets can be found here: https://api.census.gov/data.html.
+For now, the 2021 American Community Survey is being used (code acs/acs1)
+
+Variable names for acs1 data can be found here can be found here: https://api.census.gov/data/2021/acs/acs1/subject/variables.html
--- main.py
+++ main.py
@@ -28,6 +28,18 @@ def build_metro_area_json():
     with open("metro_area_codes.json", "w") as f:
         json.dump(metro_area_dict, f)
 
+def build_variable_json():
+    ''''Build a json containing selected variables'''
+    acs_var_dict = {
+        'S1701_C03_001E': 'Percent of population below poverty level',
+        'S2301_C04_001E': 'Unemployment rate (16 years and older)',
+        'S1902_C03_001E': 'Mean household income',
+        'S0102_C01_037E': 'Percent of population with bachelor\'s degree (25 years and older)'
+    }
+    with open("variable_codes.json", "w" ) as f:
+        json.dump(acs_var_dict, f)
+
+
 
 def load_metro_area_json(json_file):
     '''Loads a json file containing metro area names and locations and convers it to a dictionary'''
@@ -35,66 +47,134 @@ def load_metro_area_json(json_file):
         data = json.load(f)
     return data
 
+def load_variable_json(json_file):
+    '''Loads a json file containing ACS variable codes and convert it to a dictionary'''
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data
 
-def find_metro_area_num(city, metro_area_dict):
-    '''Return value for metro area code to be used in request given a text input.
+
+def get_user_input_metro_area(metro_area_dict):
+    '''Return value for metro area code to be used in request, given a text input.
     Value is a 3 digit number.'''
-    if city == '*':
-        return city
-    matches = []
-    mult_match_index = 0
-    mult_match_found = False
-    ma_code = ''
-    for metro_area in metro_area_dict.keys():
-        if city.lower() in metro_area.lower():
-            matches.append(metro_area)  # Append if text entry is a substring of a metro area
-    if len(matches) == 0:
-        print('Metro area not found. Please check spelling')
-        return None
-    elif len(matches) == 1:
-        ma_code = metro_area_dict[matches[0]]['id']
-    else:
-        print("Multiple matches found. Which metro area would you like to select?")
-        for match in matches:
-            print(f"[{mult_match_index}]: {match}, {metro_area_dict[match]['state']}")
-            mult_match_index +=1
-        while not mult_match_found:
-            selection = int(input('Enter value: '))
-            if 0<=selection<=mult_match_index-1:
-                ma_code=metro_area_dict[matches[selection]]['id']
-                mult_match_found = True
-            else:
-                print('Please enter a valid number.')
+    while True:
+        matches = []
+        mult_match_index = 0
+        mult_match_found = False
+        ma_code = ''
+        city = input(
+            "Please enter the name of a metro area.\nInput: ")
+        if city == '*':
+            return city
+        for metro_area in metro_area_dict.keys():
+            if city.lower() in metro_area.lower():
+                matches.append(metro_area)  # Append if text entry is a substring of a metro area
+        if len(matches) == 0:
+            print('Metro area not found. Please check spelling')
+        elif len(matches) == 1:
+            ma_code = metro_area_dict[matches[0]]['id']
+            break
+        else:
+            print("Multiple matches found. Which metro area would you like to select?")
+            for match in matches:
+                print(f"[{mult_match_index}]: {match}, {metro_area_dict[match]['state']}")
+                mult_match_index +=1
+            while not mult_match_found:
+                selection = input('Enter value: ')
+                try:
+                    ma_code=metro_area_dict[matches[int(selection)]]['id']
+                    mult_match_found = True
+                except (TypeError, IndexError, ValueError):
+                    print('Please enter a valid number.')
+            if mult_match_found:
+                break
     return ma_code
 
-def find_variable_name(var_text_entry):
-    pass
+def get_metro_area_set(metro_area_dict):
+    '''Get a list of metro areas for comparison'''
+    ma_code_set = set()
+    while True:
+        ma_code = get_user_input_metro_area(metro_area_dict)
+        ma_code_set.add(ma_code)
+        continue_input = input("Type 'Y' to add another city to your city. Otherwise, "
+                               "hit any key to finish.\nInput: ")
+        if continue_input.lower() != 'y':
+            break
+    return ma_code_set
 
 
-def build_query(variable, metro_area_input):
+def build_var_selection_dict(variable_dict):
+    '''Iterate through items in dict and assign a value to each item for use with user input.'''
+    selection_dict = {}
+    var_counter = 1
+    for item in variable_dict:
+        selection_dict[str(var_counter)] = {'code': item, 'description': variable_dict[item]}
+        var_counter +=1
+    return selection_dict
+
+def get_user_input_var(var_dict_for_selection):
+    print("Please select a variable from the list below:")
+    for item in var_dict_for_selection:
+        print("[{0}] {1}".format(item, var_dict_for_selection[item]['description']))
+    while True:
+        user_inp = input('Selection: ')
+        try:
+            var_code = var_dict_for_selection[user_inp]['code']
+            break
+        except KeyError:
+            print('Invalid entry, Ensure your entry is an integer listed in the selection below')
+    return var_code
+
+def build_query(var_code, ma_code):
     '''Takes a variable and metro area and return a request query'''
-    metro_area = find_metro_area_num(metro_area_input)
     request_str = ('/'.join((host, year, dataset,
-                             get))) + ',' + variable + '&for=' + geography + ':' + metro_area + '&key=' + api_key
+                             get))) + ',' + var_code + '&for=' + geography + ':' + ma_code + '&key=' + api_key
     return request_str
 
+def build_query_list(var_code, ma_code_set):
+    request_str_list = []
+    for ma_code in ma_code_set:
+        request_str = build_query(var_code, ma_code)
+        request_str_list.append(request_str)
+    return request_str_list
 
 def submit_query(request_str):
     response = requests.get(request_str)
     return response.json()
 
+def get_response_list(request_str_list):
+    response_list = []
+    for request_str in request_str_list:
+        response = submit_query(request_str)
+        response_list.append(response)
+    return response_list
 
-def build_table():
-    pass
+
+def parse_responses(response_list):
+    results_dict = {}
+    for item in response_list:
+        results = item[1]
+        results_dict[results[0]] = results[1]
+    return results_dict
+
+def build_results_json(results_dict):
+    return json.dumps(results_dict)
+
+def main():
+    metro_area_dict = load_metro_area_json('metro_area_codes.json') #import metro area codes
+    var_dict = load_variable_json('variable_codes.json') #import variable codes
+    var_dict_for_selection = build_var_selection_dict(var_dict) #Dict used for purposes of allowing selection via command line
+    var_code = get_user_input_var(var_dict_for_selection) #Get variable from user
+    ma_code_set = get_metro_area_set(metro_area_dict) #Get ma_list from user
+    request_str_list = build_query_list(var_code, ma_code_set) #Build all request strings
+    response_list = get_response_list(request_str_list) #Get list containing all responses
+    results_dict = parse_responses(response_list) #Build dict containing all responses
+    results_json = build_results_json(results_dict) #Return query results in JSON format
+    return results_json
 
 
 if __name__ == '__main__':
-    # request_str = build_query('S0101_C01_001E','*')
-    # response_json = submit_query(request_str)
-    # build_metro_area_json()
-    metro_area_dict = load_metro_area_json('metro_area_codes.json')
-    ma_code = find_metro_area_num('Portland', metro_area_dict)
-    print(ma_code)
+    results = main()
+    print(results)
 
 
-    #prompt user, enter * for all cities
